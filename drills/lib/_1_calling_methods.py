2c93ee02faeefd61c40c98777447dc2673e4f429
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-26 22:34:49 +0100

    boolean_to_string passed

--- drills/lib/_1_calling_methods.py
+++ drills/lib/_1_calling_methods.py
@@ -197,8 +197,7 @@ def float_is_negative(float):
 #   Call:    boolean_to_string(True)
 #   Returns: "True"
 def boolean_to_string(boolean):
-    # your code goes here (delete the pass below)
-    pass
+    return str(boolean)
 
 
 # Congrats, you're done with this file. Move on to the next one.
