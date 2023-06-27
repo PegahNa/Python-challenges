745daa4c340cea9e3f2e0b13338e269dcdf3b5d5
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-27 22:23:08 +0100

    Validate a password passed

--- program/lib/password_validator.py
+++ program/lib/password_validator.py
@@ -18,5 +18,10 @@
 # == YOUR CODE ==
 
 def is_valid(password):
-    pass # Your code goes here (delete the `pass`)
+   if len(password) > 7 and any(char in password for char in ['!', '@', '$', '%', '&']):
+       return True
+   else:
+       return False
+
+   
 
